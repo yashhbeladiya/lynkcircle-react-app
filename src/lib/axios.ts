cf1a375c43f404370aaf74442222cda9faadb50d
Yash Beladiya <yashhbeladiya@gmail.com>
2024-12-08 20:19:41 -0500

    add env var

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const SERVER_URL = process.env.SERVER_URL;
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5100/api/v1",
+    baseURL: SERVER_URL || "http://localhost:5100/api/v1",
     withCredentials: true, // send cookies when cross-domain requests are made
     headers: {
       "Content-Type": "application/json",
